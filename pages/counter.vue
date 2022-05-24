6a77e3ed2b6230b6ed1451bd783f8935c1dd6de3
tateki <tatekiobo@gmail.com>
2022-05-24 09:28:07 +0900

    counterボタンページを追加

---
+++ pages/counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>カウンター基本形</h1>
+    {{ $store.state.counter }}
+    <input type="button" value="+1" @click="increase">
+    <input type="button" value="-1" @click="decrease">
+    <input type="button" value="リセット" @click="reset">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CounterPage',
+  methods: {
+    increase () {
+      this.$store.commit('increase')
+    },
+    decrease () {
+      this.$store.commit('decrease')
+    },
+    reset () {
+      this.$store.commit('reset')
+    }
+  }
+}
+</script>
