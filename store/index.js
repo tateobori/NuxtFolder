61c1da8735617c2d625f792ae88f4f87a684b83c
tateki <tatekiobo@gmail.com>
2022-05-24 15:17:50 +0900

    add the action parts

--- store/index.js
+++ store/index.js
@@ -1,9 +1,11 @@
-
 export const state = () => ({
   counter: 0
 })
 
 export const mutations = {
+  add (state, payload) {
+    state.counter += Number(payload)
+  },
   increase (state) {
     state.counter++
   },
@@ -15,6 +17,8 @@ export const mutations = {
   }
 }
 
-//   export const actions = {
-
-//   }
+export const actions = {
+  addAction (data, payload) {
+    data.commit('add', payload)
+  }
+}
