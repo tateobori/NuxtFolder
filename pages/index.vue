bbd41a0fba4d3e53d3f1ba0c63e6ef7b7da59af1
tateki <tatekiobo@gmail.com>
2022-05-24 15:16:09 +0900

    add the index2

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,25 @@
 <template>
   <div>
-    <h1>Nuxtのトップページです。</h1>
-    <ul>
-      <li>Git</li>
-      <li>Vue</li>
-      <li>Nuxt</li>
-    </ul>
+    <h1>ストアに値を渡す</h1>
+    <p>{{ $store.state.counter }}</p>
+    <p>dataの値: {{ $data }}</p>
+    <p>入力値: <input v-model="num" type="text"></p>
+    <input type="button" value="追加" @click="add">
   </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'CounterPage',
+  data () {
+    return {
+      num: 1
+    }
+  },
+  methods: {
+    add () {
+      this.$store.commit('add', this.num)
+    }
+  }
 }
 </script>
