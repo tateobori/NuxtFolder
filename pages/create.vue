76defb1c0cc61b0b60505a330ac69eb50e1cd128
tateki <tatekiobo@gmail.com>
2022-05-24 18:13:25 +0900

    devブランチでcreateファイルを追加

---
+++ pages/create.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Nuxt.jsとrestAPIを用いてみよう</h1>
+        <p>入力値: 
+            <input type="text" v-model="task.name">
+        </p>
+        <input type="button" value="追加" @click="vuexCreate">
+        <pre>storeの値: {{ $store.state }} </pre>
+    </div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            task: {
+                id: null,
+                name: "名無しのタスク",
+                completed: false
+            }
+        }
+    },
+    methods: {
+        vuexCreate () {
+            this.$store.dispatch('createAction', this.$data.task)
+        }
+    }
+}
+</script>
