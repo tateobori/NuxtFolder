5bd7be9644b6afec6b3934153b5487d0b8ad84cd
tateki <tatekiobo@gmail.com>
2022-05-24 15:16:24 +0900

    add the index2

---
+++ pages/index2.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>ストアに値を渡す(actionを経由)</h1>
+    <p>ストアの値{{ $store.state.counter }}</p>
+    <p>dataの値: {{ $data }}</p>
+    <p>入力値: <input v-model="num" type="text"></p>
+    <input type="button" value="追加" @click="add">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CounterPage',
+  data () {
+    return {
+      num: 1
+    }
+  },
+  methods: {
+    add () {
+      this.$store.dispatch('addAction', this.num)
+    }
+  }
+}
+</script>
