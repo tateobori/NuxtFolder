88f83846cb583aaed148e684f143375088ca2102
tateki <tatekiobo@gmail.com>
2022-05-24 09:29:38 +0900

    read.vueを用意。後で読み込みファイルページとして編集

---
+++ pages/read.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Nuxtのトップページです。</h1>
+    <ul>
+      <li>Git</li>
+      <li>Vue</li>
+      <li>Nuxt</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
